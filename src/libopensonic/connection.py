8621396e6ea31b6c63a01469b390041a5d4978d9
Eric B Munson <eric@munsonfam.org>
2024-01-09 11:16:40 -0500

    Update build process
    The initial release built a library that seems to have missed all the
    code. Hopefully this has addressed the packaging problems and better
    followed more of the packaging best practices.
    
    Signed-off-by: Eric B Munson <eric@munsonfam.org>

--- src/libopensonic/connection.py
+++ src/libopensonic/connection.py
@@ -25,8 +25,7 @@ from io import StringIO
 import json
 import os
 
-from src.libopensonic.errors import *
-
+from . import errors
 
 API_VERSION = '1.16.1'
 
@@ -118,9 +117,9 @@ class Connection:
         self._netrc = None
         if useNetrc is not None:
             self._process_netrc(useNetrc)
-        elif username is None or (password is None and (salt is None or token is None)):
-            raise CredentialError('You must specify either a username/password '
-                'combination or salt/token combination or "useNetrc" must be either True or a string '
+        elif username is None or password is None:
+            raise errors.CredentialError('You must specify either a username/password '
+                'combination or "useNetrc" must be either True or a string '
                 'representing a path to a netrc file')
 
         self.setPort(port)
@@ -200,7 +199,7 @@ class Connection:
         if res['status'] == 'ok':
             return True
         elif res['status'] == 'failed':
-            exc = getExcByCode(res['error']['code'])
+            exc = errors.getExcByCode(res['error']['code'])
             raise exc(res['error']['message'])
         return False
 
@@ -484,7 +483,7 @@ class Connection:
         newerThan:int   Return matches newer than this timestamp
         """
         if artist == album == title == any == None:
-            raise ArgumentError('Invalid search.  You must supply search '
+            raise errors.ArgumentError('Invalid search.  You must supply search '
                 'criteria')
         methodName = 'search'
 
@@ -736,9 +735,9 @@ class Connection:
             songIds = []
 
         if playlistId == name == None:
-            raise ArgumentError('You must supply either a playlistId or a name')
+            raise errors.ArgumentError('You must supply either a playlistId or a name')
         if playlistId is not None and name is not None:
-            raise ArgumentError('You can only supply either a playlistId '
+            raise errors.ArgumentError('You can only supply either a playlistId '
                  'OR a name, not both')
 
         q = self._getQueryDict({'playlistId': playlistId, 'name': name})
@@ -1467,7 +1466,7 @@ class Connection:
         if action == 'add':
             # We have to deal with the sids
             if not (isinstance(sids, list) or isinstance(sids, tuple)):
-                raise ArgumentError('If you are adding songs, "sids" must '
+                raise errors.ArgumentError('If you are adding songs, "sids" must '
                     'be a list or tuple!')
             req = self._getRequestWithList(methodName, 'id', sids, q)
         else:
@@ -1672,10 +1671,10 @@ class Connection:
         try:
             rating = int(rating)
         except Exception as exc:
-            raise ArgumentError('Rating must be an integer between 0 and 5: '
+            raise errors.ArgumentError('Rating must be an integer between 0 and 5: '
                 '%r' % rating) from exc
         if rating < 0 or rating > 5:
-            raise ArgumentError('Rating must be an integer between 0 and 5: '
+            raise errors.ArgumentError('Rating must be an integer between 0 and 5: '
                 '%r' % rating)
 
         q = self._getQueryDict({'id': item_id, 'rating': rating})
@@ -2850,7 +2849,7 @@ class Connection:
         if result['status'] == 'ok':
             return True
         elif result['status'] == 'failed':
-            exc = getExcByCode(result['error']['code'])
+            exc = errors.getExcByCode(result['error']['code'])
             raise exc(result['error']['message'])
 
 
@@ -2915,7 +2914,7 @@ class Connection:
                                 netrc file to use
         """
         if not use_netrc:
-            raise CredentialError('useNetrc must be either a boolean "True" '
+            raise errors.CredentialError('useNetrc must be either a boolean "True" '
                 'or a string representing a path to a netrc file, '
                 'not {0}'.format(repr(use_netrc)))
         if isinstance(use_netrc, bool) and use_netrc:
@@ -2925,7 +2924,7 @@ class Connection:
             self._netrc = netrc(os.path.expanduser(use_netrc))
         auth = self._netrc.authenticators(self._hostname)
         if not auth:
-            raise CredentialError('No machine entry found for {0} in '
+            raise errors.CredentialError('No machine entry found for {0} in '
                 'your netrc file'.format(self._hostname))
 
         # If we get here, we have credentials
